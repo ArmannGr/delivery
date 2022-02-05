38e0aec66f95e44b32b411edd3ce49fd17bcfad9
Benyamin Radmard <b.radmard77@gmail.com>
2022-02-05 09:36:11 +0100

    Revert "Revert "First 5 levels add""
    This reverts commit 6486ec7422e2e375e876918eca669d6bb698ba57.

--- deliveryapp/src/Pages/checkout/Checkout.js
+++ deliveryapp/src/Pages/checkout/Checkout.js
@@ -8,13 +8,13 @@ import ClockLoader from "react-spinners/BeatLoader";
 import LoadingOverlay from "react-loading-overlay";
 
 export default function Checkout() {
-    const [cart, setCart] = useState([]);
+    const [carts, setCarts] = useState([]);
     const {currentUser} = useAuth();
-    const [restaurant, setRestaurant] = useState([]);
+    const [restaurants, setRestaurants] = useState([]);
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        getCartByUID(currentUser.uid, setCart,setRestaurant);
+        getCartByUID(currentUser.uid, setCarts, setRestaurants);
 
     }, []);
 
@@ -24,13 +24,13 @@ export default function Checkout() {
             active={loading}
             spinner={<ClockLoader color={"#fff"}/>}
         >
+
             <section className="offer-dedicated-body mt-4 mb-4 pt-2 pb-2 ">
-                <Row className="justify-content-md-center">
-                    <Col md={4}>
-                        <CheckoutOrderOverview cart={cart} restaurant={restaurant} setLoading={setLoading}
-                                               setCart={setCart} uid={currentUser.uid}/>
-                    </Col>
-                </Row>
+                {carts && restaurants && carts.map((value, index) => {
+                    if(restaurants.length === 0 || carts.length === 0) return;
+                    return<CheckoutOrderOverview cart={value} restaurant={restaurants[index]} setLoading={setLoading}
+                                                 setCart={setCarts} uid={currentUser.uid}/>;
+                })}
 
 
             </section>
