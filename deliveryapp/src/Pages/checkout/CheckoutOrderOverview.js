6486ec7422e2e375e876918eca669d6bb698ba57
Benyamin Radmard <b.radmard77@gmail.com>
2022-02-05 09:30:19 +0100

    Revert "First 5 levels add"
    This reverts commit 3816b170450f1c765dc2a28712b2a8a5f6c108ec.

--- deliveryapp/src/Pages/checkout/CheckoutOrderOverview.js
+++ deliveryapp/src/Pages/checkout/CheckoutOrderOverview.js
@@ -1,36 +1,21 @@
 import {Button, Col, Form, Image, InputGroup, OverlayTrigger, Tooltip} from "react-bootstrap";
 import Icofont from "react-icofont";
 import CheckoutItem from "../../common/CheckoutItem";
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import PrettifyAddress from "../../utils/AddressPrettifier";
 import {IncrementItem, DecreaseItem} from "../Detail/cartService";
 import {db, doc, setDoc} from "../../firebase";
 import {useHistory} from "react-router-dom";
 
 export function CheckoutOrderOverview(props) {
-
-    const [cart, setCart] = useState([]);
-    const [totalToPay, setTotalToPay] = useState([]);
-
-
+    const totalToPay = props.cart.reduce((a, b) => a + (b.price * b.quantity), 0);
     const history = useHistory();
-
-    useEffect(() => {
-        setTotalToPay(cart.reduce((a, b) => a + (b.price * b.quantity), 0));
-    }, [cart]);
-
-
-    useEffect(() => {
-            setCart(props.cart);
-        },
-        []);
-
 
 
     async function ProcessPayment() {
         props.setLoading(true);
         //Change order state to paid
-        const orderID = `${props.uid}${props.restaurant.restaurantID}`;
+        const orderID = `${props.uid}${props.restaurant[0].restaurantID}`;
         const orderDoc = await setDoc(doc(db, `orders/${orderID}`), {
             payment: {
                 hasPaid: true,
@@ -43,34 +28,33 @@ export function CheckoutOrderOverview(props) {
 
         props.setLoading(false);
         history.push("/trackorder", {
-            restaurantID: props.restaurant.restaurantID,
-            uid: props.uid
+                restaurantID:props.restaurant[0].restaurantID,
+                uid:props.uid
         });
     }
 
     return <>
         <div className="generator-bg rounded shadow-sm mb-4 p-4 osahan-cart-item">
-            <div
-                className="d-flex mb-4 osahan-cart-item-profile">
-                <Image fluid className="mr-3 rounded-pill" alt="osahan" src={props.restaurant.profileImage}/>
-                <div className="d-flex flex-column">
-                    <h6 className="mb-1 text-white">{props.restaurant.name}
-                    </h6>
-                    <p className="mb-0 text-white"><Icofont
-                        icon="location-pin"/> {PrettifyAddress(props.restaurant.address)}
-                    </p>
+            {props.restaurant && props.restaurant.map(restaurant => <div
+                    className="d-flex mb-4 osahan-cart-item-profile">
+                    <Image fluid className="mr-3 rounded-pill" alt="osahan" src={restaurant.profileImage}/>
+                    <div className="d-flex flex-column">
+                        <h6 className="mb-1 text-white">{restaurant.name}
+                        </h6>
+                        <p className="mb-0 text-white"><Icofont icon="location-pin"/> {PrettifyAddress(restaurant.address)}
+                        </p>
+                    </div>
                 </div>
-            </div>
-
+            )}
             <div className="bg-white rounded shadow-sm mb-2">
-                {cart && cart.map(item => {
+                {props.cart && props.cart.map(item => {
                         return <CheckoutItem
                             itemName={item.name}
                             price={item.price * item.quantity}
                             priceUnit="€"
                             qty={item.quantity}
-                            increase={() => IncrementItem(item.name, props.setLoading, props.restaurant.restaurantID, props.uid, setCart)}
-                            decrease={() => DecreaseItem(item.name, props.setLoading, props.restaurant.restaurantID, props.uid, setCart)}
+                            increase={() => IncrementItem(item.name, props.setLoading, props.restaurant[0].restaurantID, props.uid, props.setCart)}
+                            decrease={() => DecreaseItem(item.name, props.setLoading, props.restaurant[0].restaurantID, props.uid, props.setCart)}
                         />;
                     }
                 )
