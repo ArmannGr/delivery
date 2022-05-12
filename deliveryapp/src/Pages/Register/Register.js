78fb8931c105d614624585d189236bc29bad207b
Benyamin Radmard <b.radmard77@gmail.com>
2022-05-12 12:47:19 +0200

    New Register Form

--- deliveryapp/src/Pages/Register/Register.js
+++ deliveryapp/src/Pages/Register/Register.js
@@ -1,86 +1,171 @@
-import React, {Component, useRef, useState} from 'react';
-import {Link} from 'react-router-dom';
-import {Row, Col, Container, Form, Button, Alert} from 'react-bootstrap';
-import {useAuth} from '../../contexts/AuthContext';
-import {useHistory} from "react-router-dom";
+import React, {useRef, useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import {Button, Col, Container, Form, Row} from 'react-bootstrap';
+import {useAuth} from "../../contexts/AuthContext";
+import LoadingOverlay from "react-loading-overlay";
+import ClockLoader from "react-spinners/BeatLoader";
 
 
-class Register extends Component {
-	/*
-        const emailRef = useRef()
-        const passwordRef = useRef()
-        const {signUp} = useAuth()
-        const [error, setError] = useState("")
-        const [loading, setLoading] = useState(false)
-        const history = useHistory();
+function Register() {
+    const {signUp} = useAuth()
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
+    const history = useHistory();
+    const [validated, setValidated] = useState(false);
 
-        async function handleSubmit(e){
+    async function handleSubmit(e) {
+        e.preventDefault();
+        setLoading(true);
+        const formElements = document.forms['registration'];
+        if (formElements.checkValidity() === false) {
             e.preventDefault();
-            console.log("Working");
+            e.stopPropagation();
+            setLoading(false);
+            setValidated(true);
+        } else {
+            setValidated(false);
             try {
-                setLoading(true);
-                await signUp(emailRef.current.value, passwordRef.current.value);
+                const formData = {
+                    address: {
+                        street: formElements.elements['street'].value,
+                        houseNumber: formElements.elements['houseNumber'].value,
+                        postalCode: formElements.elements['postalCode'].value,
+                        city: formElements.elements['city'].value,
+                    },
+                    name: formElements.elements['name'].value,
+                    email: formElements.elements['email'].value,
+                    number: formElements.elements['number'].value,
+                    password: formElements.elements['password'].value
+                };
+                await signUp(formData);
                 history.push('/');
-            }catch(e) {
+            } catch (e) {
                 console.log(e.toString());
                 setError('Failed to create an account!');
             }
             setLoading(false);
-        }*/
-	async handleSubmit(e) {
-		e.preventDefault();
-		const {item} = this.state;
+        }
+    }
 
-		await fetch('/api/user/create', {
-			method: 'PUT',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(item),
-		});
-	}
+    return (
+        <LoadingOverlay
+            active={loading}
+            spinner={<ClockLoader color={"#fff"}/>}
+        >
+            <Container fluid className='bg-white'>
+                <Col>
+                    <div className="login d-flex align-items-center py-5">
+                        <Container>
+                            <Row>
+                                <Col md={9} lg={8} className="mx-auto pl-5 pr-5">
+                                    <h3 className="login-heading mb-4">New Buddy!</h3>
 
-	render() {
-		const {item} = this.state;
-		return (
-			<Container fluid className='bg-white'>
-				<Row>
-					<Col md={4} lg={6} className="d-none d-md-flex bg-image"></Col>
-					<Col md={8} lg={6}>
-						<div className="login d-flex align-items-center py-5">
-							<Container>
-								<Row>
-									<Col md={9} lg={8} className="mx-auto pl-5 pr-5">
-										<h3 className="login-heading mb-4">New Buddy!</h3>
+                                    <Form onSubmit={handleSubmit} id="registration" noValidated validated={validated}>
+                                        <Form.Text className='font-weight-bold h5'>Persönliche Daten</Form.Text>
 
-										<Form onSubmit={this.handleSubmit}>
-											<div className="form-label-group">
-												<Form.Control type="email" id="inputEmail" placeholder="Email address"
-															  />
-												<Form.Label htmlFor="inputEmail">Email address / Mobile</Form.Label>
-											</div>
-											<div className="form-label-group">
-												<Form.Control type="password" id="inputPassword" placeholder="Password"
-															  />
-												<Form.Label htmlFor="inputPassword">Password</Form.Label>
-											</div>
-											<Button type="submit"
-													className="btn btn-lg btn-outline-primary btn-block btn-login text-uppercase font-weight-bold mb-2">Sign
-												Up</Button>
-											<div className="text-center pt-3">
-												Already have an account? <Link className="font-weight-bold" to="/login">Sign
-												In</Link>
-											</div>
-										</Form>
-									</Col>
-								</Row>
-							</Container>
-						</div>
-					</Col>
-				</Row>
-			</Container>
-		);
-	}
+                                        <Form.Group className="form-label-group">
+                                            <Form.Control required id='name' type='text'
+                                                          placeholder='Vor- und Nachnamen eingeben'/>
+                                            <Form.Label htmlFor="name">Vor- und Nachname</Form.Label>
+                                            <Form.Control.Feedback type="invalid">Name ist
+                                                erforderlich</Form.Control.Feedback>
+                                        </Form.Group>
+
+                                        <Form.Group className="form-label-group">
+                                            <Form.Control required id='email' type='text'
+                                                          placeholder='E-Mail eingeben'/>
+                                            <Form.Label htmlFor="email">E-Mail</Form.Label>
+                                            <Form.Control.Feedback type="invalid">E-mail ist
+                                                erforderlich</Form.Control.Feedback>
+                                        </Form.Group>
+
+                                        <Form.Group className="form-label-group">
+                                            <Form.Control required id='number' type='number'
+                                                          placeholder='Telefonnummer eingeben'/>
+                                            <Form.Label htmlFor="number">Telefonnummer</Form.Label>
+                                            <Form.Control.Feedback type="invalid">Telefonnummer ist
+                                                erforderlich</Form.Control.Feedback>
+                                        </Form.Group>
+
+                                        <Form.Group className="form-label-group">
+                                            <Form.Control required type="password" id="password"
+                                                          placeholder="Password"/>
+                                            <Form.Label htmlFor="inputPassword">Password</Form.Label>
+                                        </Form.Group>
+
+
+                                        <Form.Text className='font-weight-bold h5'>Adresse</Form.Text>
+
+                                        <Form.Row>
+                                            <Col>
+                                                <Form.Group className="form-label-group">
+
+                                                    <Form.Control required id='street' type='text'
+                                                                  placeholder='Straße eingeben'/>
+                                                    <Form.Label htmlFor="street"
+                                                                className='text-muted'>Straße</Form.Label>
+                                                    <Form.Control.Feedback type="invalid">Straße ist
+                                                        erforderlich</Form.Control.Feedback>
+                                                </Form.Group>
+                                            </Col>
+                                            <Col>
+                                                <Form.Group className="form-label-group">
+
+                                                    <Form.Control required id='houseNumber' type='text'
+                                                                  placeholder='Hausnummer eingeben'/>
+                                                    <Form.Label htmlFor="houseNumber"
+                                                                className='text-muted'>Hausnummer</Form.Label>
+                                                    <Form.Control.Feedback type="invalid">Hausnummer ist
+                                                        erforderlich</Form.Control.Feedback>
+                                                </Form.Group>
+                                            </Col>
+                                        </Form.Row>
+
+                                        <Form.Row>
+
+                                            <Col>
+                                                <Form.Group className="form-label-group">
+
+                                                    <Form.Control required id='postalCode' type='number'
+                                                                  placeholder='Postleitzahl eingeben'/>
+                                                    <Form.Label htmlFor="postalCode"
+                                                                className='text-muted'>PLZ</Form.Label>
+                                                    <Form.Control.Feedback type="invalid">Postleitzahl ist
+                                                        erforderlich</Form.Control.Feedback>
+                                                </Form.Group>
+                                            </Col>
+
+                                            <Col>
+                                                <Form.Group className="form-label-group">
+
+                                                    <Form.Control required id='city' type='text'
+                                                                  placeholder='Stadt eingeben'/>
+                                                    <Form.Label htmlFor="city"
+                                                                className='text-muted'>Stadt</Form.Label>
+                                                    <Form.Control.Feedback type="invalid">
+                                                        Stadtname ist erforderlich
+                                                    </Form.Control.Feedback>
+                                                </Form.Group>
+                                            </Col>
+
+                                        </Form.Row>
+
+
+                                        <Button type="submit"
+                                                className="btn btn-lg  btn-block btn-login text-uppercase font-weight-bold mb-2">Registrieren</Button>
+                                        <div className="text-center pt-3">
+                                            Ich habe bereits ein Konto!<Link className="font-weight-bold"
+                                                                             to="/login">Einloggen</Link>
+                                        </div>
+                                    </Form>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </div>
+                </Col>
+            </Container>
+        </LoadingOverlay>
+    );
 }
+
 export default Register;
